437629306313e947aa6d8592e8b277d664cdd7c5
Nasiruddin Abubakar <111733598+nasiruddinabubakar@users.noreply.github.com>
2023-05-14 18:25:09 +0500

    l

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-const renderCountry = function (data3) {
-  //console.log(data3.languages);
-  const language = data3.languages ? data3.languages[0].name : "Unknown";
+const renderCountry = function (data3, classname = '') {
+  console.log('lang in render',data3.languages);
+  // const language = data3.languages ? data3.languages.name : "Unknown";
   const htmll = `
-  <article class="country">
+  <article class=  "${'country'} ${classname}" >
     <img class="country__img" src="${data3.flag}" />
     <div class="country__data">
       <h3 class="country__name">${data3.name}</h3>
@@ -15,7 +15,7 @@ const renderCountry = function (data3) {
       <p class="country__row"><span>👫</span>${+(
         data3.population / 1000000
       ).toFixed(1)}</p>
-      <p class="country__row"><span>🗣️</span>${language}</p>
+      <p class="country__row"><span>🗣️</span>${data3.languages[0].name}</p>
       <p class="country__row"><span>💰</span>${data3.currencies[0].name}</p>
     </div>
   </article>
@@ -40,18 +40,20 @@ function countrySinfo(countryname) {
     renderCountry(data);
 
     const [neighbor] = data.borders;
-  console.log(neighbor);
+  console.log("neighbor", neighbor, data.borders);
     if (!data.borders) return;
 
     const request2 = new XMLHttpRequest();
-    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbor}`);
+    request2.open("GET", `https://restcountries.com/v2/name/${neighbor}`);
      request2.send();
 
       request2.addEventListener('load',function(){
 
         const [data2] = JSON.parse(this.responseText);
-        console.log(data2);
-        renderCountry(data2);
+        
+        console.log("Neibour detail", data2);
+        //data2.languages= Object.values(data2.languages);
+        renderCountry(data2,'neighbour');
 
 
 
